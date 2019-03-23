642167d3d5a636278538f50f8eb5ddc8bb5facc0
KaustubhPatange <developerkp16@gmail.com>
2019-03-23 20:07:05 +0530

    Updated my project list

--- js/myjs.js
+++ js/myjs.js
@@ -6,3 +6,23 @@ function ButtonClicked(url,id) {
     window.open(url)
     $(id).modal('hide');
 }
+
+function hidethis(button) {
+  document.getElementById('wrapper').style.display = 'none';
+}
+
+function hidethis1(button) {
+  document.getElementById('wrapper').style.display = 'flex';
+}
+
+$('.viewmore1').hover(
+  function() {
+      var $this = $(this); // caching $(this)
+      $this.data('defaultText', $this.text());
+      $this.text("SURE!");
+  },
+  function() {
+      var $this = $(this); // caching $(this)
+      $this.text($this.data('defaultText'));
+  }
+);
